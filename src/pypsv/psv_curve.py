3ef75194c00bd20874a16131b791eb66fd48f2c5
Maximilian Schanner <arthus@gfz-potsdam.de>
2025-01-10 15:36:52 +0100

    Increase prior variance for simple prior.

--- src/pypsv/psv_curve.py
+++ src/pypsv/psv_curve.py
@@ -15,7 +15,7 @@ from .calibration_curves import intcal20, shcal20, marine20
 
 JITTER = 1e-4
 AXIAL_DIPOLE_PRIOR = -30
-DEFAULT_PRIOR_VALUES = (8.25, 150)
+DEFAULT_PRIOR_VALUES = (15, 150)
 MODEL_INCREASE_FACTOR = 2
 
 age_types = [
